3625bedd76a6f09e473f39821175402f7de3e45a
TomDvpmt <tbsuiviactu@gmail.com>
2023-11-08 16:59:53 +0100

    feat: Timber + basic twig templates

--- web/app/themes/wptraining-theme/index.php
+++ web/app/themes/wptraining-theme/index.php
@@ -1,1 +1,10 @@
-Salut !
+<?php
+
+use Timber\Timber;
+
+$context = Timber::context();
+
+$context['message'] = "I am a message from php sent to the twig file.";
+dump($context);
+
+Timber::render('pages/index.twig', $context);
