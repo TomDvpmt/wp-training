b290353210036a1de0d9260abf40279b8a5be51e
TomDvpmt <tbsuiviactu@gmail.com>
2023-11-09 18:00:46 +0100

    feat: product brand taxonomy + ACF for select brand

--- web/app/themes/wptraining-theme/functions.php
+++ web/app/themes/wptraining-theme/functions.php
@@ -13,8 +13,37 @@ function mytheme_init()
 {
     register_post_type('product', [
         'label' => 'Product',
+        'labels' => [
+            'name' => 'Produits',
+            'singular_name' => 'Produit',
+            'add_new' => 'Ajouter un produit',
+            'add_new_item' => 'Ajouter un produit'
+        ],
         'public' => true,
         'supports' => ['title', 'editor', 'thumbnail']
+    ]);
+
+    register_taxonomy('brand', 'product', [
+        'public' => true,
+        'show_admin_column' => true,
+        'show_in_quick_edit' => true,
+        'meta_box_cb' => false,
+        'hierarchical' => true,
+        'labels' => [
+            'name' => 'Marques',
+            'singular_name' => 'Marque',
+            'search_items' => 'Chercher des marques',
+            'popular_items' => 'Marques populaires',
+            'all_items' => 'Toutes les marques',
+            'edit_item' => 'Éditer la marque',
+            'view_item' => 'Voir la marque',
+            'update_item' => 'Mettre à jour la marque',
+            'add_new_item' => 'Ajouter une marque',
+            'new_item_name' => 'Nouveau nom de marque',
+            'separate_items_with_commas' => 'Séparez les marques avec des virgules',
+            'choose_from_most_used' => 'Choisissez parmi les plus utilisées',
+            'not_found' => 'Aucune marque trouvée.',
+        ]
     ]);
 }
 
